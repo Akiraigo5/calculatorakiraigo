5ae11e23a80a4ff4c15955780bb12522c0121143
Akiraigo5 <Akiraigo5@gmail.com>
2020-04-20 17:37:20 +0300

    Add files via upload

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,128 +1,106 @@
-package com.neko.calculatorakiraigo
+package com.neko.calculatoreasyakiraigo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.view.View
-import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.TextView
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import net.objecthunter.exp4j.ExpressionBuilder
 
-const val HUNDRED_PERCENT = 100.00
-const val TIP_INCREMENT_PERCENT = 5
-const val PEOPLE_INCREMENT_VALUE = 1
-const val MIN_TIP = 0
-const val MIN_PEOPLE = 2
-const val MAX_TIP = 95
-const val MAX_PEOPLE = 20
 
-class MainActivity : AppCompatActivity(), View.OnClickListener, TextWatcher {
-
-    // Top Section
-    private lateinit var expensePerPersonTextView: TextView
-    private lateinit var billEditText: EditText
-
-    // Tip Section
-    private lateinit var addTipButton: ImageButton
-    private lateinit var tipTextView: TextView
-    private lateinit var subtractTipButton: ImageButton
-
-    // Number of People Section
-    private lateinit var addPeopleButton: ImageButton
-    private lateinit var numberOfPeopleTextView: TextView
-    private lateinit var subtractPeopleButton: ImageButton
-
-    private var numberOfPeople = 4 // set default number of people
-    private var tipPercent = 20 // set default tip percent
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        initViews()
-    }
 
-    private fun initViews() {
-        expensePerPersonTextView = findViewById(R.id.expensePerPersonTextView)
-        billEditText = findViewById(R.id.billEditText)
-
-        addTipButton = findViewById(R.id.addTipButton)
-        tipTextView = findViewById(R.id.tipTextView)
-        subtractTipButton = findViewById(R.id.subtractTipButton)
-
-        addPeopleButton = findViewById(R.id.addPeopleButton)
-        numberOfPeopleTextView = findViewById(R.id.numberOfPeopleTextView)
-        subtractPeopleButton = findViewById(R.id.subtractPeopleButton)
-
-        addTipButton.setOnClickListener(this)
-        subtractTipButton.setOnClickListener(this)
-
-        addPeopleButton.setOnClickListener(this)
-        subtractPeopleButton.setOnClickListener(this)
-
-        billEditText.addTextChangedListener(this)
-    }
-
-
-    private fun calculateExpense() {
-
-        val totalBill = billEditText.text.toString().toDouble()
-
-        val totalExpense = ((HUNDRED_PERCENT + tipPercent) / HUNDRED_PERCENT) * totalBill
-        val individualExpense = totalExpense / numberOfPeople
-
-        expensePerPersonTextView.text = String.format("₴%.2f", individualExpense)
-
-    }
-
-    override fun onClick(v: View?) {
-        when (v?.id) {
-            R.id.addTipButton -> incrementTip()
-            R.id.subtractTipButton -> decrementTip()
-            R.id.addPeopleButton -> incrementPeople()
-            R.id.subtractPeopleButton -> decrementPeople()
+        //button
+        val mStartActBtn = findViewById<Button>(R.id.startActBtn)
+        //handle button click
+        mStartActBtn.setOnClickListener {
+            //start activity intent
+            startActivity(Intent(this@MainActivity, NewActivity::class.java))
         }
-    }
 
-    private fun incrementTip() {
-        if (tipPercent != MAX_TIP) {
-            tipPercent += TIP_INCREMENT_PERCENT
-            tipTextView.text = String.format("%d%%", tipPercent)
+
+
+
+
+
+
+//Numbers
+        tvOne.setOnClickListener { appendOnExpresstion("1", true) }
+        tvTwo.setOnClickListener { appendOnExpresstion("2", true) }
+        tvThree.setOnClickListener { appendOnExpresstion("3", true) }
+        tvFour.setOnClickListener { appendOnExpresstion("4", true) }
+        tvFive.setOnClickListener { appendOnExpresstion("5", true) }
+        tvSix.setOnClickListener { appendOnExpresstion("6", true) }
+        tvSeven.setOnClickListener { appendOnExpresstion("7", true) }
+        tvEight.setOnClickListener { appendOnExpresstion("8", true) }
+        tvNine.setOnClickListener { appendOnExpresstion("9", true) }
+        tvZero.setOnClickListener { appendOnExpresstion("0", true) }
+        tvDot.setOnClickListener { appendOnExpresstion(".", true) }
+
+        //Operators
+        tvPlus.setOnClickListener { appendOnExpresstion("+", false) }
+        tvMinus.setOnClickListener { appendOnExpresstion("-", false) }
+        tvMul.setOnClickListener { appendOnExpresstion("*", false) }
+        tvDivide.setOnClickListener { appendOnExpresstion("/", false) }
+        tvOpen.setOnClickListener { appendOnExpresstion("(", false) }
+        tvClose.setOnClickListener { appendOnExpresstion(")", false) }
+        tvPercent.setOnClickListener { appendOnExpresstion("%", false) }
+
+        tvClear.setOnClickListener {
+            tvExpression.text = ""
+            tvResult.text = ""
         }
-        calculateExpense()
-    }
 
-    private fun decrementTip() {
-        if (tipPercent != MIN_TIP) {
-            tipPercent -= TIP_INCREMENT_PERCENT
-            tipTextView.text = String.format("%d%%", tipPercent)
+        tvBack.setOnClickListener {
+            val string = tvExpression.text.toString()
+            if (string.isNotEmpty()) {
+                tvExpression.text = string.substring(0, string.length - 1)
+            }
+            tvResult.text = ""
         }
-        calculateExpense()
-    }
 
-    private fun incrementPeople() {
-        if (numberOfPeople != MAX_PEOPLE) {
-            numberOfPeople += PEOPLE_INCREMENT_VALUE
-            numberOfPeopleTextView.text = numberOfPeople.toString()
+        tvEquals.setOnClickListener {
+            try {
+
+                val expression = ExpressionBuilder(tvExpression.text.toString()).build()
+                val result = expression.evaluate()
+                val longResult = result.toLong()
+
+                if (result == longResult.toDouble())
+                    tvResult.text = longResult.toString()
+                else
+                    tvResult.text = result.toString()
+
+            } catch (e: Exception) {
+                Log.d("Exception", " message : " + e.message)
+            }
         }
-        calculateExpense()
+
     }
 
-    private fun decrementPeople() {
-        if (numberOfPeople != MIN_PEOPLE) {
-            numberOfPeople -= PEOPLE_INCREMENT_VALUE
-            numberOfPeopleTextView.text = numberOfPeople.toString()
+    fun appendOnExpresstion(string: String, canClear: Boolean) {
+
+        if (tvResult.text.isNotEmpty()) {
+            tvExpression.text = ""
         }
-        calculateExpense()
-    }
 
-    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-        if (!billEditText.text.isEmpty()) {
-            calculateExpense()
+        if (canClear) {
+            tvResult.text = ""
+            tvExpression.append(string)
+        } else {
+            tvExpression.append(tvResult.text)
+            tvExpression.append(string)
+            tvResult.text = ""
+
+
+            }
+
         }
-    }
 
-    override fun afterTextChanged(s: Editable?) {}
-    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-}
+    }
